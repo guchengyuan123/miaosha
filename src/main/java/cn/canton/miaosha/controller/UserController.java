d2c656bd52a236804766ad2ad4074ec473abbe8f
chengyuan.gu <chengyuan.gu@ele.me>
2018-05-31 23:30:44 +0800

    秒杀商品 v1.1 压测+war包

---
+++ src/main/java/cn/canton/miaosha/controller/UserController.java
@@ -0,0 +1,29 @@
+package cn.canton.miaosha.controller;
+
+import cn.canton.miaosha.domain.MiaoshaUser;
+import cn.canton.miaosha.redis.RedisService;
+import cn.canton.miaosha.result.Result;
+import cn.canton.miaosha.service.MiaoshaUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+    MiaoshaUserService userService;
+	
+	@Autowired
+    RedisService redisService;
+	
+    @RequestMapping("/info")
+    @ResponseBody
+    public Result<MiaoshaUser> info(Model model, MiaoshaUser user) {
+        return Result.success(user);
+    }
+    
+}
