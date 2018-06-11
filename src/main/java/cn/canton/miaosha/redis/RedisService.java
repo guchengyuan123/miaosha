a779e838b1343e705dd31a7ac3896bb2510d03a6
guchengyuan123 <31217223+guchengyuan123@users.noreply.github.com>
2018-06-11 23:07:15 +0800

    秒杀商品 v1.4 增加对象缓存

--- src/main/java/cn/canton/miaosha/redis/RedisService.java
+++ src/main/java/cn/canton/miaosha/redis/RedisService.java
@@ -1,8 +1,10 @@
 package cn.canton.miaosha.redis;
 
-import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSON;
+
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
@@ -12,8 +14,10 @@ public class RedisService {
     @Autowired
     JedisPool jedisPool;
 
-
-    public <T> T get(KeyPrefix prefix, String key, Class<T> clazz){
+    /**
+     * 获取当个对象
+     */
+    public <T> T get(KeyPrefix prefix, String key, Class<T> clazz) {
         Jedis jedis = null;
         try {
             jedis = jedisPool.getResource();
@@ -22,102 +26,130 @@ public class RedisService {
             String str = jedis.get(realKey);
             T t = stringToBean(str, clazz);
             return t;
-        }finally {
+        } finally {
             returnToPool(jedis);
         }
     }
 
-    public <T> boolean set(KeyPrefix prefix, String key, T value){
+    /**
+     * 设置对象
+     */
+    public <T> boolean set(KeyPrefix prefix, String key, T value) {
         Jedis jedis = null;
         try {
             jedis = jedisPool.getResource();
             String str = beanToString(value);
-            if (str==null||str.length()<=0){
+            if (str == null || str.length() <= 0) {
                 return false;
             }
             //生成真正的key
             String realKey = prefix.getPrefix() + key;
             int seconds = prefix.expireSeconds();
-            if (seconds <= 0){
-                jedis.set(realKey,str);
-            }else {
-                jedis.setex(realKey,seconds,str);
+            if (seconds <= 0) {
+                jedis.set(realKey, str);
+            } else {
+                jedis.setex(realKey, seconds, str);
             }
             return true;
-        }finally {
+        } finally {
             returnToPool(jedis);
         }
     }
 
-    public <T> boolean exists(KeyPrefix prefix, String key){
+    /**
+     * 判断key是否存在
+     */
+    public <T> boolean exists(KeyPrefix prefix, String key) {
         Jedis jedis = null;
         try {
             jedis = jedisPool.getResource();
             //生成真正的key
             String realKey = prefix.getPrefix() + key;
             return jedis.exists(realKey);
-        }finally {
+        } finally {
             returnToPool(jedis);
         }
     }
 
-    public <T> Long incr(KeyPrefix prefix, String key){
+    /**
+     * 删除
+     */
+    public boolean delete(KeyPrefix prefix, String key) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            //生成真正的key
+            long ret = jedis.del(key);
+            return ret > 0;
+        } finally {
+            returnToPool(jedis);
+        }
+    }
+
+    /**
+     * 增加值
+     */
+    public <T> Long incr(KeyPrefix prefix, String key) {
         Jedis jedis = null;
         try {
             jedis = jedisPool.getResource();
             //生成真正的key
             String realKey = prefix.getPrefix() + key;
             return jedis.incr(realKey);
-        }finally {
+        } finally {
             returnToPool(jedis);
         }
     }
 
-    public <T> Long decr(KeyPrefix prefix, String key){
+    /**
+     * 减少值
+     */
+    public <T> Long decr(KeyPrefix prefix, String key) {
         Jedis jedis = null;
         try {
             jedis = jedisPool.getResource();
             //生成真正的key
             String realKey = prefix.getPrefix() + key;
             return jedis.decr(realKey);
-        }finally {
+        } finally {
             returnToPool(jedis);
         }
     }
 
     private <T> String beanToString(T value) {
-        if (value==null){
+        if (value == null) {
             return null;
         }
         Class<?> clazz = value.getClass();
-        if (clazz == int.class || clazz == Integer.class){
-            return ""+value;
-        }else if (clazz == String.class){
-            return (String)value;
-        }else if (clazz == long.class || clazz == Long.class){
-            return ""+value;
-        }else {
+        if (clazz == int.class || clazz == Integer.class) {
+            return "" + value;
+        } else if (clazz == String.class) {
+            return (String) value;
+        } else if (clazz == long.class || clazz == Long.class) {
+            return "" + value;
+        } else {
             return JSON.toJSONString(value);
         }
     }
 
+    @SuppressWarnings("unchecked")
     private <T> T stringToBean(String str, Class<T> clazz) {
-        if (str == null || str.length() <= 0 || clazz == null){
+        if (str == null || str.length() <= 0 || clazz == null) {
             return null;
         }
-        if (clazz == int.class || clazz == Integer.class){
+        if (clazz == int.class || clazz == Integer.class) {
             return (T) Integer.valueOf(str);
-        }else if (clazz == String.class){
+        } else if (clazz == String.class) {
             return (T) str;
-        }else if (clazz == long.class || clazz == Long.class){
+        } else if (clazz == long.class || clazz == Long.class) {
             return (T) Long.valueOf(str);
-        }else {
-            return JSON.toJavaObject(JSON.parseObject(str),clazz);
+        } else {
+            return JSON.toJavaObject(JSON.parseObject(str), clazz);
         }
     }
 
     private void returnToPool(Jedis jedis) {
-        if (jedis != null){
+        if (jedis != null) {
             jedis.close();
         }
     }
--- src/main/java/cn/canton/miaosha/service/MiaoshaUserService.java
+++ src/main/java/cn/canton/miaosha/service/MiaoshaUserService.java
@@ -1,5 +1,8 @@
 package cn.canton.miaosha.service;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import cn.canton.miaosha.dao.MiaoshaUserDao;
 import cn.canton.miaosha.domain.MiaoshaUser;
 import cn.canton.miaosha.exception.GlobalException;
@@ -13,11 +16,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
 
 @Service
 public class MiaoshaUserService {
+
 
     public static final String COOKIE_NAME_TOKEN = "token";
 
@@ -27,26 +29,69 @@ public class MiaoshaUserService {
     @Autowired
     RedisService redisService;
 
-    public MiaoshaUser getById(long id){
-        return miaoshaUserDao.getById(id);
+    public MiaoshaUser getById(long id) {
+        //取缓存
+        MiaoshaUser user = redisService.get(MiaoshaUserKey.getById, "" + id, MiaoshaUser.class);
+        if (user != null) {
+            return user;
+        }
+        //取数据库
+        user = miaoshaUserDao.getById(id);
+        if (user != null) {
+            redisService.set(MiaoshaUserKey.getById, "" + id, user);
+        }
+        return user;
     }
 
+    // http://blog.csdn.net/tTU1EvLDeLFq5btqiK/article/details/78693323
+    public boolean updatePassword(String token, long id, String formPass) {
+        //取user
+        MiaoshaUser user = getById(id);
+        if (user == null) {
+            throw new GlobalException(CodeMsg.MOBILE_NOT_EXIST);
+        }
+        //更新数据库
+        MiaoshaUser toBeUpdate = new MiaoshaUser();
+        toBeUpdate.setId(id);
+        toBeUpdate.setPassword(MD5Util.formPassToDBPass(formPass, user.getSalt()));
+        miaoshaUserDao.update(toBeUpdate);
+        //处理缓存
+        redisService.delete(MiaoshaUserKey.getById, "" + id);
+        user.setPassword(toBeUpdate.getPassword());
+        redisService.set(MiaoshaUserKey.token, token, user);
+        return true;
+    }
+
+
+    public MiaoshaUser getByToken(HttpServletResponse response, String token) {
+        if (StringUtils.isEmpty(token)) {
+            return null;
+        }
+        MiaoshaUser user = redisService.get(MiaoshaUserKey.token, token, MiaoshaUser.class);
+        //延长有效期
+        if (user != null) {
+            addCookie(response, token, user);
+        }
+        return user;
+    }
+
+
     public String login(HttpServletResponse response, LoginVo loginVo) {
-        if (loginVo == null){
+        if (loginVo == null) {
             throw new GlobalException(CodeMsg.SERVER_ERROR);
         }
-        String mobile =  loginVo.getMobile();
+        String mobile = loginVo.getMobile();
         String formPass = loginVo.getPassword();
         //判断手机号是否存在
         MiaoshaUser user = getById(Long.parseLong(mobile));
-        if (user == null){
+        if (user == null) {
             throw new GlobalException(CodeMsg.MOBILE_NOT_EXIST);
         }
         //验证密码
         String dbPass = user.getPassword();
         String saltDB = user.getSalt();
-        String calcPass = MD5Util.formPassToDBPass(formPass,saltDB);
-        if (!calcPass.equals(dbPass)){
+        String calcPass = MD5Util.formPassToDBPass(formPass, saltDB);
+        if (!calcPass.equals(dbPass)) {
             throw new GlobalException(CodeMsg.PASSWORD_ERROR);
         }
         //生成cookie
@@ -55,23 +100,12 @@ public class MiaoshaUserService {
         return token;
     }
 
-    public MiaoshaUser getByToken(HttpServletResponse response, String token) {
-        if (StringUtils.isEmpty(token)){
-            return null;
-        }
-        MiaoshaUser user = redisService.get(MiaoshaUserKey.token,token,MiaoshaUser.class);
-        //延长有效期
-        if (user != null){
-            addCookie(response, token, user);
-        }
-        return user;
-    }
-
-    private void addCookie(HttpServletResponse response, String token, MiaoshaUser user){
-        redisService.set(MiaoshaUserKey.token,token,user);
-        Cookie cookie = new Cookie(COOKIE_NAME_TOKEN,token);
+    private void addCookie(HttpServletResponse response, String token, MiaoshaUser user) {
+        redisService.set(MiaoshaUserKey.token, token, user);
+        Cookie cookie = new Cookie(COOKIE_NAME_TOKEN, token);
         cookie.setMaxAge(MiaoshaUserKey.token.expireSeconds());
         cookie.setPath("/");
         response.addCookie(cookie);
     }
+
 }
