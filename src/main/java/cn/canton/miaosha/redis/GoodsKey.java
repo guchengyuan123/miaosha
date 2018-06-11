fd46376eca6e2d589aa2de7184fad3b3d54b1b35
chengyuan.gu <chengyuan.gu@ele.me>
2018-06-11 14:06:42 +0800

    秒杀商品 v1.3 加入页面缓存

---
+++ src/main/java/cn/canton/miaosha/redis/GoodsKey.java
@@ -0,0 +1,10 @@
+package cn.canton.miaosha.redis;
+
+public class GoodsKey extends BasePrefix{
+
+	private GoodsKey(int expireSeconds, String prefix) {
+		super(expireSeconds, prefix);
+	}
+	public static GoodsKey getGoodsList = new GoodsKey(60, "goodsList");
+	public static GoodsKey getGoodsDetail = new GoodsKey(60, "goodsDetail");
+}
