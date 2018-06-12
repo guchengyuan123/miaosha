f05a12ead0b1560ffafd1b3dce9398bd9de141e5
guchengyuan123 <31217223+guchengyuan123@users.noreply.github.com>
2018-06-12 23:23:29 +0800

    秒杀商品 v2.0 大版本修改，完成MQ前代码，整理代码格式

--- src/main/java/cn/canton/miaosha/redis/GoodsKey.java
+++ src/main/java/cn/canton/miaosha/redis/GoodsKey.java
@@ -6,6 +6,6 @@ public class GoodsKey extends BasePrefix {
         super(expireSeconds, prefix);
     }
 
-    public static GoodsKey getGoodsList = new GoodsKey(60, "gl");
-    public static GoodsKey getGoodsDetail = new GoodsKey(60, "gd");
+    public static GoodsKey getGoodsList = new GoodsKey(60, "goodsList");
+    public static GoodsKey getGoodsDetail = new GoodsKey(60, "goodsDetail");
 }
